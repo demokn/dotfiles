da2ac175f6f282c37b0ed4a4d4119f13453ef415
demo_knyang <demo.knyang@gmail.com>
2021-10-16 15:14:34 +0800

    move xbindkeysrc to XDG_CONFIG_HOME directory

--- .config/X11/xinitrc.d/dwm/dwm.sh
+++ .config/X11/xinitrc.d/dwm/dwm.sh
@@ -4,5 +4,6 @@ setbg &
 # 解决首次进入 dwm 壁纸显示不完全, 有黑块的问题
 (sleep 10 ; setbg) &
 dunst &
-xbindkeys &
+# default is $HOME/.xbindkeysrc
+xbindkeys -f ${XDG_CONFIG_HOME:-$HOME/.config}/xbindkeys/config &
 fcitx5 &
