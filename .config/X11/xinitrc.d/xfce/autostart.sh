06cebf1a77bb2b120aab317389f53ddc9666a6c6
demo_knyang <demo.knyang@gmail.com>
2021-10-17 14:23:40 +0800

    move xinitrc to XDG_CONFIG_HOME directory

--- .config/X11/xinitrc.d/xfce/autostart.sh
+++ .config/X11/xinitrc.d/xfce/autostart.sh
@@ -3,7 +3,8 @@ plank &
 # continuous file synchronization program
 syncthing -no-browser &
 # grabbing keys program for X
-xbindkeys &
+# default is $HOME/.xbindkeysrc
+xbindkeys -f ${XDG_CONFIG_HOME:-$HOME/.config}/xbindkeys/config &
 # notification service for the Xfce Desktop
 # try to kill dunst first (used in dwm)
 killall dunst ; /usr/lib/xfce4/notifyd/xfce4-notifyd &
